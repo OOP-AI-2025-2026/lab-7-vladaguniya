29e6232373fa036b0e797acf352506a543624cb8
vladaguniya <9100299@stud.op.edu.ua>
2025-12-02 01:29:31 +0200

    Create Launcher.java

---
+++ src/main/java/ua/opnu/Launcher.java
@@ -0,0 +1,8 @@
+package ua.opnu;
+
+public class Launcher {
+    public static void main(String[] args) {
+        // Передаємо керування в SortingList, інакше не стартує
+        SortingList.main(args);
+    }
+}
